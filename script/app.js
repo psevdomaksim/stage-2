6786e185befd58f4a99d07cc260e8aa55f4660d0
psevdomaksim <varmax2004@gmail.com>
2024-09-06 16:12:34 +0300

    Added animation of the side panel and JS logic for its opening

--- script/app.js
+++ script/app.js
@@ -1,11 +1,30 @@
 const burgerBtn = document.getElementById("hamburger_button");
-const burgerMenu = document.getElementById("burger_menu");
-burgerBtn.onclick = () => {
-  console.log(burgerMenu.style);
+const burger = document.getElementById("burger");
 
-  if (burgerMenu.style.visibility == "visible") {
-    burgerMenu.style.visibility = "hidden";
+const brg_line_1 = document.getElementById("brg_line_1");
+const brg_line_2 = document.getElementById("brg_line_2");
+const brg_line_3 = document.getElementById("brg_line_3");
+
+burgerBtn.onclick = () => {
+  if (burger.style.visibility == "visible") {
+    burger.style.visibility = "hidden";
+    burger.style.top = "-740px";
+
+    burgerBtn.style.color = "#191e1e";
+    burgerBtn.style.transform = "none";
+
+    brg_line_1.style.transform = "none";
+    brg_line_2.style.transform = "none";
+    brg_line_3.style.transform = "none";
   } else {
-    burgerMenu.style.visibility = "visible";
+    burger.style.visibility = "visible";
+    burger.style.top = "0";
+    
+    burgerBtn.style.color = "white";
+    burgerBtn.style.transform = "rotate(-90deg)";
+
+    brg_line_1.style.transform = "rotate(-45deg) translate(-4.93px, 4.93px)";
+    brg_line_2.style.transform = "scaleX(0)";
+    brg_line_3.style.transform = "rotate(45deg) translate(-4.93px, -4.93px)";
   }
 };
